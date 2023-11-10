22b3132b4a17b7f4159c191b5df1c7d7f57a7fd5
luc <luc.spielvogel@syntax-institut.de>
2023-11-10 13:55:58 +0100

    first student commit

--- B2C_IOS_VariablenUndDatentypen/main.swift
+++ B2C_IOS_VariablenUndDatentypen/main.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+
+//Schreibe hier deinen Code für Aufgabe 1:
+
+    //Deine Erklärung:
+
+
+
+//Schreibe hier deinen Code für Aufgabe 2:
+
+
+
+//Schreibe hier deinen Code für Aufgabe 3:
+
+
+
+//Schreibe hier deinen Code für Aufgabe 4:
