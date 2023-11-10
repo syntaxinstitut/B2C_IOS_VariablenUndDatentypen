d1e538c4a9857c02cd6a9e2af3d5ff4cc4b3afe6
luc <luc.spielvogel@syntax-institut.de>
2023-11-10 13:53:51 +0100

    first solution commit

--- B2C_IOS_VariablenUndDatentypen/main.swift
+++ B2C_IOS_VariablenUndDatentypen/main.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+//Schreibe hier deinen Code für Aufgabe 1:
+    var alter: Int = 31
+    let maxGeschwindigkeit: Double = 130.0
+    //Deine Erklärung:
+    //Im logischen Zusammenhang ändert sich das Alter jedes Jahr. Die Maximalgeschwindidkeit bleibt allerdings gleich!
+
+
+
+//Schreibe hier deinen Code für Aufgabe 2:
+    var begrüßung: String = "Hallo, Welt!"
+    begrüßung = "Hallo, Swift!"
+
+
+
+//Schreibe hier deinen Code für Aufgabe 3:
+    var istBenutzerAngemeldet: Bool = false
+    istBenutzerAngemeldet = true
+
+
+
+//Schreibe hier deinen Code für Aufgabe 4:
+    let vorname: String = "Max"
+    let nachname: String = "Mustermann"
+    var vollständigerName: String = vorname + nachname
